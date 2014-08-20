c680f603bc4b4c5e1545aaef4b7bd7991d51feae
Steven Bird <stevenbird1@gmail.com>
2014-08-20 20:44:46 +0400

    updates for NLTK 3

--- book/pylisting/code_evaluate.py
+++ book/pylisting/code_evaluate.py
@@ -3,6 +3,6 @@
 def evaluate(text, segs):
     words = segment(text, segs)
     text_size = len(words)
-    lexicon_size = len(' '.join(set(words)))
+    lexicon_size = sum(len(word) + 1 for word in words)
     return text_size + lexicon_size
 
--- book/pylisting/code_unusual.py
+++ book/pylisting/code_unusual.py
@@ -3,6 +3,6 @@
  def unusual_words(text):
      text_vocab = set(w.lower() for w in text if w.isalpha())
      english_vocab = set(w.lower() for w in nltk.corpus.words.words())
-     unusual = text_vocab.difference(english_vocab)
+     unusual = text_vocab - english_vocab
      return sorted(unusual)
 
