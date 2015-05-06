6be5ffb15cef27f69fe64c642b441bdbbe2ec817
Steven Bird <stevenbird1@gmail.com>
2015-05-06 12:57:44 +1000

    deploy

--- book_2ed/pylisting/code_gender_features_overfitting.py
+++ book_2ed/pylisting/code_gender_features_overfitting.py
@@ -5,7 +5,7 @@ def gender_features2(name):
     features["first_letter"] = name[0].lower()
     features["last_letter"] = name[-1].lower()
     for letter in 'abcdefghijklmnopqrstuvwxyz':
-        features["count(%s)" % letter] = name.lower().count(letter)
-        features["has(%s)" % letter] = (letter in name.lower())
+        features["count({})".format(letter)] = name.lower().count(letter)
+        features["has({})".format(letter)] = (letter in name.lower())
     return features
 
