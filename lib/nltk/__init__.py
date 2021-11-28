10666b94597a661c72f262b9bab1a231862c302c
Danny Sepler <dannysepler@gmail.com>
2021-11-28 13:31:01 -0500

    Fix links to nltk book

--- lib/nltk/__init__.py
+++ lib/nltk/__init__.py
@@ -13,7 +13,7 @@ for Natural Language Processing.  A free online book is available.
 
 Steven Bird, Ewan Klein, and Edward Loper (2009).
 Natural Language Processing with Python.  O'Reilly Media Inc.
-http://nltk.org/book
+http://nltk.org/book/
 
 isort:skip_file
 """
